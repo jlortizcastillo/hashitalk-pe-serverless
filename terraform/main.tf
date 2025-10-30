349e44daeff717fdd17b5695b8c24b78bbfb0f07
Jorge Luis Ortiz <jorgeluisortizcastillo@gmail.com>
2025-10-29 23:22:12 -0500

    Adding dynamodb table in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,29 @@
 # --- /terraform/main.tf ---
+
+# Locals para Tablas de DynamoDB
+locals {
+  table_name = "example-${var.environment}"
+  table_billing_mode = ""
+}
+
+resource "aws_dynamodb_table" "example_table" {
+  name         = local.table_name
+  billing_mode = local.table_billing_mode
+  hash_key     = "id"
+  range_key    = "fechaCreacion"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "fechaCreacion"
+    type = "S"
+  }
+
+  tags = {
+    Name        = local.table_name
+    Environment = var.environment
+  }
+}
