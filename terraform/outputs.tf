a9aaf9c3d0f0e346d9124fdc8a08ff7e0ed2c98c
Jorge Luis Ortiz <jorgeluisortizcastillo@gmail.com>
2026-01-03 14:30:39 -0500

    Agregando outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,20 @@
 # --- /terraform/outputs.tf ---
+
+# API GATEWAY
+output "api_gateway_rest_api_execution_arn" {
+  value = aws_api_gateway_rest_api.rest_api.execution_arn
+}
+
+# DYNAMODB
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.example_table.name
+}
+
+# LAMBDA
+output "lambda_function_name" {
+  value = aws_lambda_function.lambda_function.function_name
+}
+
+output "lambda_iam_policy_name" {
+  value = aws_iam_policy.lambda_iam_policy.name
+}
